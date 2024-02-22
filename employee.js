ea0502e2282231c8dad8144dad5d9a10a73fe68d
Anand . <anandsmathpati18@gmail.com>
2024-02-22 12:57:51 +0530

    UC4 COMPLETED SUCCEFULLY

--- employee.js
+++ employee.js
@@ -1,41 +1,35 @@
-let attendence = '';
+let wagePerHour = 20;
+let fullTimeWorkingHours = 8;
+let partTimeWorkingHours = 4;
+let dailyEmployeeWage = 0;
+let attendance = '';
 
 function generateRandomBinary() {
-    attendence += Math.random() < 0.5 ? '0' : '1';
+    attendance += Math.random() < 0.5 ? '0' : '1';
 }
 
 generateRandomBinary();
 
-function checkAttenndence(attendence) {
-    switch (attendence) {
+function checkAttendance(attendance) {
+    switch (attendance) {
         case '1':
             console.log("EMPLOYEE IS PRESENT");
-
-            let wageperhour = 20;
-            let oneworkingday = 8;
-            let dailyEmployeeWage = wageperhour * oneworkingday;
-            console.log("full time employee wage " + dailyEmployeeWage);
-
-            let wageperhourpart = 20;
-            let oneworkingdaypart = 4;
-            let dailyEmployeeWagepart = wageperhourpart * oneworkingdaypart;
-            console.log("part time employee wage " + dailyEmployeeWagepart);
+            calculateDailyEmployeeWage(fullTimeWorkingHours);
             break;
-
-        default:
+        case '0':
             console.log("EMPLOYEE IS NOT PRESENT");
-
-            let wageperhourAbsent = 20;
-            let oneworkingdayAbsent = 0;
-            let dailyEmployeeWageAbsent = wageperhourAbsent * oneworkingdayAbsent;
-            console.log("full time employee wage " + dailyEmployeeWageAbsent);
-
-            let wageperhourpartAbsent = 20;
-            let oneworkingdaypartAbsent = 0;
-            let dailyEmployeeWagepartAbsent = wageperhourpartAbsent * oneworkingdaypartAbsent;
-            console.log("part time employee wage " + dailyEmployeeWagepartAbsent);
+            break;
+        default:
+            console.log("INVALID ATTENDANCE DATA");
             break;
     }
 }
 
-checkAttenndence(attendence);
+function calculateDailyEmployeeWage(workingHours) {
+    dailyEmployeeWage = wagePerHour * workingHours;
+    console.log("Full-time employee wage: " + dailyEmployeeWage);
+    dailyEmployeeWage = partTimeWorkingHours * wagePerHour;
+    console.log("Part-time employee wage: " + dailyEmployeeWage);
+}
+
+checkAttendance(attendance);
