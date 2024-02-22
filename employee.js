84dfae152d08f1f4d717cfa577da0ad49b546ff4
Anand . <anandsmathpati18@gmail.com>
2024-02-22 15:13:33 +0530

    UC5 completed

--- employee.js
+++ employee.js
@@ -1,53 +1,43 @@
+function employeeeWage() {
+    var daynmonths = 30;
+    var day = 1;
+    var parttimedays = 0;
+    var fulltimedays = 0;
+    var totalmonthwage = 0;
+    var totalhours = 0;
 
-function employeeeWage(){
-    var daynmonths=30;
-    var day=1;
-    var parttimedays=0;
-    var fulltimedays=0;
-    var totalmonthwage=0;
-    var totalhours=0;
+    while (day <= daynmonths) { 
       
-    while(day<=daynmonths){
-        let presentday;
-        let perhourwage=20;
-        let fulltimehours=8;
-        let fulltimewage=fulltimehours*perhourwage
-        let parttimewage=4*perhourwage;
+        let perhourwage = 20;
+        let fulltimehours = 8;
+        let fulltimewage = fulltimehours * perhourwage;
+        let parttimewage = 4 * perhourwage;
 
+        let attendance = Math.random() < 0.5 ? '0' : '1';
 
-        let attendance=()=>{return Math.round(Math.random()*1)}
-    
+        if (attendance == '1') {
+            
+            
+            let parttimehours = Math.round(Math.random() * 4);
+            if (parttimehours == 4) {
+                let wage = parttimewage;
+                totalmonthwage += wage;
+                parttimedays++;
+                totalhours += 4;
+            } else {
+                let wage = fulltimewage;
+                totalmonthwage += wage;
+                fulltimedays++;
+                totalhours += 8;
+            }
+        }
 
-    if(attendance==1){
-        presentday++;
-   
-    
-
-    let parttimehours=Math.round(Math.random()*4);
-    if(parttimehours==4){
-        let wage=parttimewage;
-        totalmonthwage+=wage;
-        parttimedays++;
-        totalhours+=4;
-    }
-    else{
-        let wage=fulltimewage;
-        totalmonthwage+=wage;
-        fulltimedays++;
-        totalhours+=8;
+        day++;
     }
 
-    }
-  
-
-    day++
-
+    console.log("Employee has worked " + parttimedays + " part-time days and " + fulltimedays + " full-time days");
+    return `Total wage is ${totalmonthwage}`;
 }
-     return " total wage is "+totalmonthwage
 
-}
-  let res =employeeeWage()
-  console.log(res);
-
-
-
+let res = employeeeWage();
+console.log(res);
